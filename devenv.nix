cef3f86b9ad5f12f636abf5bd0cb4df70b39825a
TimotheeRen <timothee.ren@proton.me>
2026-09-26 15:08:03 +0200

    chore: add kube toolchain

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,13 @@
 {
   env.GREET = "devenv";
 
-  packages = [ pkgs.git ];
+  packages = [ 
+    pkgs.k3d 
+    pkgs.kubernetes-helm
+    pkgs.fluxcd
+    pkgs.kubectl
+    pkgs.k9s
+  ];
 
   languages.rust = {
     enable = true;
